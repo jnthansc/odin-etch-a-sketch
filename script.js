01d2b49e4afe35db9f5162414afee3dc1a7d8dbf
Jonathan Schmidt <jona.schmidt99@web.de>
2024-07-13 20:35:56 +0200

    Add custom grid sizing
    Add a new button for setting a custom size of the grid to draw on. Done
    by dynamically calculating the correspondig relative width of the
    squares.

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+const button = document.querySelector('.dimensions');
+
+button.addEventListener('click', (event) => {
+  const numberOfSquaresPerSide = prompt(
+    'Enter the number of squares per side (max 100)',
+    0
+  );
+
+  buildGrid(numberOfSquaresPerSide);
+});
+
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
+function buildGrid(numberOfSquaresPerSide) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
-  square.addEventListener('mouseover', (event) => {
-    square.classList.add('hovered');
-  });
+  numberOfSquaresPerSide =
+    numberOfSquaresPerSide > 100 ? 100 : numberOfSquaresPerSide;
+
+  const totalSquares = Math.pow(numberOfSquaresPerSide, 2);
+  const relativeWidth = Math.floor((100 / numberOfSquaresPerSide) * 100) / 100;
+
+  for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.flexBasis = `${relativeWidth}%`;
+    container.appendChild(square);
+
+    square.addEventListener('mouseover', (event) => {
+      square.classList.add('hovered');
+    });
+  }
 }
+
+buildGrid(16);
